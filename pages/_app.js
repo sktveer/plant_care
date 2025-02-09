bf2f65369b898a7a6a9d67858faa959d0d559aea
sktveer <reachoutveer@gmail.com>
2025-02-09 09:55:10 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../index.css'; // Import Tailwind CSS here
-import '../pages/index.css'; // For any existing styles in `index.css`
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
